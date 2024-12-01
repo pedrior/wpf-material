a4777f3d46e07b89c1e25bdf7023ec06e98fcc2c
Pedro Júnior <pedrojdev@gmail.com>
2024-12-01 17:13:40 -0300

    remove unused types

--- src/WPF.Material/Extensions/DesignTimeExtensions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WPF.Material.Extensions;
-
-internal static class DesignTimeExtensions
-{
-    public static bool IsInDesignMode(this DependencyObject element) =>
-        DesignerProperties.GetIsInDesignMode(element);
-}
