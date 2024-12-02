9c1880115b2ed86e7f07abea09a3259ae868ed1a
Pedro Júnior <pedrojdev@gmail.com>
2024-12-01 21:15:41 -0300

    add missing xml doc to the `ShapeFamily` type

--- src/WPF.Material/Shapes/ShapeFamily.cs
+++ src/WPF.Material/Shapes/ShapeFamily.cs
@@ -1,17 +1,17 @@
 ﻿namespace WPF.Material.Shapes;
 
 /// <summary>
-/// 
+/// Identifies the families that a shape can belong to.
 /// </summary>
 public enum ShapeFamily
 {
     /// <summary>
-    /// 
+    /// Represents a shape with rounded corners.
     /// </summary>
     Rounded,
     
     /// <summary>
-    /// 
+    /// Represents a shape with cut corners.
     /// </summary>
     Cut
 }
