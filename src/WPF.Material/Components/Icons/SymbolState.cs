b7eb9e2aab2a9012167dd4b411d6e5203b8e059a
Pedro Júnior <pedrojdev@gmail.com>
2024-12-29 11:21:39 -0300

    add missing xml doc

--- src/WPF.Material/Components/Icons/SymbolState.cs
+++ src/WPF.Material/Components/Icons/SymbolState.cs
@@ -1,10 +1,32 @@
 ﻿namespace WPF.Material.Components;
 
+/// <summary>
+/// Specifies the states associated with a symbol.
+/// </summary>
 public enum SymbolState
 {
+    /// <summary>
+    /// Indicates no state.
+    /// </summary>
     None,
+    
+    /// <summary>
+    /// Indicates a rest symbol.
+    /// </summary>
     Rest,
+    
+    /// <summary>
+    /// Indicates a hover symbol.
+    /// </summary>
     Hover,
+    
+    /// <summary>
+    /// Indicates a press symbol.
+    /// </summary>
     Press,
+    
+    /// <summary>
+    /// Indicates a select symbol.
+    /// </summary>
     Select
 }
